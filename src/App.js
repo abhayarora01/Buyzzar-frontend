39b10f2255c11753b4ae5bb6be169ec71765cab9
abhayarora01 <aroraabhay390@gmail.com>
2025-12-01 16:23:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,56 +12,70 @@ import { useDispatch } from 'react-redux';
 import { setUserDetails } from './store/userSlice';
 
 function App() {
-  const dispatch = useDispatch()
-  const [cartProductCount,setCartProductCount] = useState(0)
+  const dispatch = useDispatch();
+  const [cartProductCount, setCartProductCount] = useState(0);
 
-  const fetchUserDetails = async()=>{
-      const dataResponse = await fetch(SummaryApi.current_user.url,{
-        method : SummaryApi.current_user.method,
-        credentials : 'include'
-      })
+  // ✅ Add token header
+  const getAuthHeader = () => {
+    const token = JSON.parse(localStorage.getItem('userInfo'))?.token;
+    return token ? { Authorization: `Bearer ${token}` } : {};
+  };
 
-      const dataApi = await dataResponse.json()
+  const fetchUserDetails = async () => {
+    try {
+      const dataResponse = await fetch(SummaryApi.current_user.url, {
+        method: SummaryApi.current_user.method,
+        headers: {
+          "Content-Type": "application/json",
+          ...getAuthHeader()  // 🔑 send token
+        },
+      });
 
-      if(dataApi.success){
-        dispatch(setUserDetails(dataApi.data))
+      const dataApi = await dataResponse.json();
+
+      if (dataApi.success) {
+        dispatch(setUserDetails(dataApi.data));
       }
-  }
+    } catch (err) {
+      console.error("fetchUserDetails error:", err);
+    }
+  };
 
-  const fetchUserAddToCart = async()=>{
-    const dataResponse = await fetch(SummaryApi.addToCartProductCount.url,{
-      method : SummaryApi.addToCartProductCount.method,
-      credentials : 'include'
-    })
+  const fetchUserAddToCart = async () => {
+    try {
+      const dataResponse = await fetch(SummaryApi.addToCartProductCount.url, {
+        method: SummaryApi.addToCartProductCount.method,
+        headers: {
+          "Content-Type": "application/json",
+          ...getAuthHeader()  // 🔑 send token
+        },
+      });
 
-    const dataApi = await dataResponse.json()
+      const dataApi = await dataResponse.json();
+      setCartProductCount(dataApi?.data?.count || 0);
+    } catch (err) {
+      console.error("fetchUserAddToCart error:", err);
+    }
+  };
 
-    setCartProductCount(dataApi?.data?.count)
-  }
+  useEffect(() => {
+    fetchUserDetails();
+    fetchUserAddToCart();
+  }, []);
 
-  useEffect(()=>{
-    /**user Details */
-    fetchUserDetails()
-    /**user Details cart product */
-    fetchUserAddToCart()
-
-  },[])
   return (
     <>
       <Context.Provider value={{
-          fetchUserDetails, // user detail fetch 
-          cartProductCount, // current user add to cart product count,
-          fetchUserAddToCart
+        fetchUserDetails,
+        cartProductCount,
+        fetchUserAddToCart
       }}>
-        <ToastContainer 
-          position='top-center'
-        />
-        
-        <Header/>
+        <ToastContainer position='top-center' />
+        <Header />
         <main className='min-h-[calc(100vh-120px)] pt-20'>
-          <Outlet/>
+          <Outlet />
         </main>
-        <Footer/>
+        <Footer />
       </Context.Provider>
     </>
   );
