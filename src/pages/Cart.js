8e5b92b8ee1498273f45dd21f4492aec1fba5835
abhayarora01 <aroraabhay390@gmail.com>
2025-12-01 16:11:59 +0530

    Update Cart.js

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -6,7 +6,7 @@ import { MdDelete } from "react-icons/md";
 import { toast } from 'react-toastify';
 import loadRazorpayScript from '../common/loadRazorpay';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://buyzzar-backend-beta.vercel.app";
 const user = JSON.parse(localStorage.getItem("user")) || {};
 
 const Cart = () => {
